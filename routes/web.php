e7946e8a9a63cd9f03d73e11a5de657c15a96a60
abdulshakur <abdulshakur486@gmail.com>
2022-08-18 22:58:23 +0300

    completed route.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,21 +14,33 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [PostsController::class, 'index'])->name('home');
+
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+
+    // render create page
+    Route::get('/posts/create', [PostsController::class, 'create'])->name('home.create');
+
+    // Post user's post
+    Route::post('/posts', [PostsController::class, 'store'])->name('home.create.a.post');
+
+    // Show edit post
+    Route::get('/posts/{post}/edit', [PostsController::class, 'edit'])->name('home.edit');
+
+    // PUT user's post
+    Route::put('/posts/{post}', [PostsController::class, 'update'])->name('edit.update');
+
+    // DELETE user's post
+    Route::delete('/posts/{post}', [PostsController::class, 'delete']);
+
+    // render manage posts
+    Route::get('/posts/manage', [PostsController::class, 'manage'])->name('mangage');
+
+    // render listing page
+    Route::get('/posts/{post}', [PostsController::class, 'show']);
 });
