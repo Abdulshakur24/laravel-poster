f139393cf1b72073f2688412b16ef3212bad0837
abdulshakur <abdulshakur486@gmail.com>
2022-08-19 17:21:21 +0300

    pinia plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,12 +6,10 @@ import { createInertiaApp } from "@inertiajs/inertia-vue3";
 import { InertiaProgress } from "@inertiajs/progress";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
-
-const appName =
-    window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
+import { createPinia } from "pinia";
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
@@ -19,6 +17,7 @@ createInertiaApp({
         ),
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
+            .use(createPinia())
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .mount(el);
