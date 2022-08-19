b3a4952a027ccf7687f33fb26f14c9b471cb8c30
abdulshakur <abdulshakur486@gmail.com>
2022-08-19 22:58:46 +0300

    customed

--- resources/js/Pages/PrivacyPolicy.vue
+++ resources/js/Pages/PrivacyPolicy.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { Head } from "@inertiajs/inertia-vue3";
-import JetAuthenticationCardLogo from "@/Components/AuthenticationCardLogo.vue";
+import JetApplicationLogo from "@/Components/ApplicationLogo.vue";
+import ThemeProvider from "@/Layouts/ThemeProvider.vue";
 
 defineProps({
     policy: String,
@@ -8,20 +9,24 @@ defineProps({
 </script>
 
 <template>
-    <Head title="Privacy Policy" />
+    <ThemeProvider>
+        <Head title="Privacy Policy" />
 
-    <div class="font-sans text-gray-900 antialiased">
-        <div class="pt-4 bg-gray-100">
-            <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
-                <div>
-                    <JetAuthenticationCardLogo />
-                </div>
-
+        <div class="font-sans text-gray-900 antialiased">
+            <div class="pt-4 bg-gray-100 dark:bg-slate-800">
                 <div
-                    class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose"
-                    v-html="policy"
-                />
+                    class="min-h-screen flex flex-col items-center pt-6 sm:pt-0"
+                >
+                    <div>
+                        <JetApplicationLogo class="max-w-[64px]" />
+                    </div>
+
+                    <div
+                        class="w-full sm:max-w-2xl mt-6 p-6 bg-white dark:bg-slate-900 shadow-md overflow-hidden prose"
+                        v-html="policy"
+                    />
+                </div>
             </div>
         </div>
-    </div>
+    </ThemeProvider>
 </template>
